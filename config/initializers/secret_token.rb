dd6b00ee249d9f1446aeeb6902f32c7e1798f36c
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-10-06 12:28:23 -0700

    added basic gems and fixed database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaxesForCaitlin::Application.config.secret_token = '008cb656ab72a6843a3aa7b60b5da44f5280ee51a4125e7feccff6dd004a3fb7331babe05aaa6db7aab83f056ff8a6f1739cc838f3ad273766e575eecb38ac48'
